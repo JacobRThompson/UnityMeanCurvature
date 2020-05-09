abcfd67b79fec8a67d293bf56b89e828884610fd
JacobRThompson <56117788+JacobRThompson@users.noreply.github.com>
2020-05-08 20:57:22 -0500

    Initial commit

---
+++ Assets/Examples/Scripts/EField/EFieldTerrainSettings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace MarchingCubes.EField
+{
+    /// <summary>
+    /// The procedural terrain generation settings
+    /// </summary>
+    [Serializable]
+    public struct EFieldTerrainSettings
+    {
+        ///<summary> The permittivity of free space, ε₀ </summary>
+        public float Epsilon { get => _epsilon; set => _epsilon = value; }
+        [SerializeField] private float _epsilon;
+        
+        /// <summary> The potential value across the entire generated surface </summary>
+        public float IsosurfacePotential { get => _isosurfacePotential; set => _isosurfacePotential = value; }
+        [SerializeField] private float _isosurfacePotential;
+
+        /// <summary> The maximum curvature deviation </summary>
+        public float MaxCurvatureDeviation { get => _maxCurvatureDeviation; set => _maxCurvatureDeviation = value; }
+        [SerializeField] private float _maxCurvatureDeviation;
+
+        ///<summary> describes the dimensions of the area in which we preform marching cubes </summary> 
+        public Vector3 Bounds {
+            get { return new Vector3(_boundsX, _boundsY, _boundsZ);}
+            set {
+                _boundsX=value.x;
+                _boundsY=value.y;
+                _boundsZ=value.z;
+            }
+        }
+        
+        public float BoundsX { get => _boundsX; set => _boundsX = value; }
+        public float BoundsY { get => _boundsY; set => _boundsY = value; }
+        public float BoundsZ { get => _boundsZ; set => _boundsZ = value; }
+        [SerializeField] private float _boundsX;
+        [SerializeField] private float _boundsY;
+        [SerializeField] private float _boundsZ;
+
+        /// <summary> Constructor </summary>
+        /// <param name="epsilon">The permittivity of free space, ε₀ </param>
+        /// <param name="isosurfacePotential">The potential value across the entire generated surface</param>
+        /// <param name="maxCurvatureDeviation">ADD DESCRIPTION</param>
+        public EFieldTerrainSettings(float epsilon, float isosurfacePotential, float maxCurvatureDeviation,
+            Vector3 bounds)
+        {
+            this._epsilon = epsilon;
+            this._isosurfacePotential   = isosurfacePotential;
+            this._maxCurvatureDeviation = maxCurvatureDeviation;
+            this.Bounds = bounds;
+
+        }
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# Marching-Cubes-Terrain
+This is my implementation of the Marching Cubes algorithm using Unity.
+
+Demo video: https://www.youtube.com/watch?v=Tge_kwjj2So
+
+## Features
+  - **Unity Job System with the Burst Compiler**
+  - Realtime terrain editing
+  - Smooth the terrain by holding the left ctrl key and left click
+  - Two world types:
+    - Procedurally-generated infinite world
+    - Import from black-and-white heightmap texture
+
+## System Requirements
+  - Unity3D (I'm developing this project with version **2019.2.2f1**, but any newer version should also work)
+
+## Setup
+  1. Clone the repository with `git clone https://github.com/Eldemarkki/Marching-Cubes-Terrain.git`
+  2. Open the project with Unity
+ 
+## Credits:
+- Paul Bourke for an example implementation of the Marching Cubes algorithm (http://paulbourke.net/geometry/polygonise/)
