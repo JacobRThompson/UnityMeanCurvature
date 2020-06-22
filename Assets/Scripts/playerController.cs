317c78939b16aa614b09c60dba8f281b04bd53de
JacobRThompson <56117788+JacobRThompson@users.noreply.github.com>
2020-06-22 05:38:01 -0500

    User-friendliness Updates
    For the folks visiting from github

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,43 @@
+﻿// allows the user to look around in play mode. Uses standard WASD controlls and shift/space to
+// raise and lower camera
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    [Range(0.0f,4.0f)] public float pitchSensitivity;
+    [Range(0.0f,4.0f)] public float yawSensitivity;
+    [Range(0.0f,0.5f)] public float moveSensitivity;
+
+    private float yaw, pitch;
+    // Update is called once per frame
+
+    private void Start() {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // get changes to camera direction from mouse axis
+        yaw += yawSensitivity * Input.GetAxis("Mouse X");
+        pitch -= pitchSensitivity * Input.GetAxis("Mouse Y");
+    
+        
+        Vector3 deltaPos=Vector3.zero;
+        
+        // get changes to camera position from key inputs
+        if (Input.GetKey("w")){deltaPos+=transform.forward;}
+        if (Input.GetKey("a")){deltaPos-=transform.right;}
+        if (Input.GetKey("s")){deltaPos-=transform.forward;}
+        if (Input.GetKey("d")){deltaPos+=transform.right;}
+
+        if (Input.GetKey("space")){deltaPos+=Vector3.up;}
+        if (Input.GetKey("right shift")||Input.GetKey("left shift")){deltaPos+=Vector3.down;}
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        transform.position+=moveSensitivity* deltaPos;
+    }
+}
--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 
- # Mean Curvature Using Unity Shaders
+ # A Unity Shader for Mean Curvature
 
-The Unity project used to write a paper of the same name for completion of a
-bachelor of mathematics. The paper can be found
-[here](https://www.example.com/), while a commentary-free demonstration can be
-found found [here(MP4)](https://github.com/JacobRThompson/MeanCurvatureShader/raw/master/Recordings/Animation.mp4) and
-[here(YouTube)](https://www.example.com/).
 
-## Video Explanation/Guide:
 
-<a href="http://www.youtube.com/watch?feature=player_embedded&v=YOUTUBE_VIDEO_ID_HERE
-" target="_blank"><img src="http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg" 
-alt="IMAGE ALT TEXT HERE" width="240" height="180" border="10" /></a>
+
+
+This was my senior seminar project to fulfill the requirements for my bachelor
+of mathematics. The accompanying paper can be found
+[here](https://www.example.com/), while a demonstration of the
+too can be found [here](https://github.com/JacobRThompson/MeanCurvatureShader/raw/master/Recordings/Animation.mp4).
+
+
+<img src="http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg" 
+alt="Demonstration Image" width="240" height="180" border="10" /></a>
 
 
 
